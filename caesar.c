48548d2b5147b527ce4834d45cf44c4de59bcc26
Bruno Vinícius Melo Tenório <btenorio2903@gmail.com>
2026-04-19 18:25:16 -0300

    Aí ss papaaaai

---
+++ caesar.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "lib/cs50.h"
+
+int main(int argc, string argv[])
+{
+    int key = 0;
+
+    if (argc != 2)
+    {
+        printf("Como usar: ./caesar key\n");
+        return 1;
+    }
+
+    for (int i = 0; i < (int) strlen(argv[1]); i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Como usar: ./caesar key\n");
+            return 1;
+        }
+        key = (key * 10) + (argv[1][i] - '0');
+    }
+
+    if(key <= 0)
+    {
+        printf("Como usar: ./caesar key\n");
+        return 1;
+    }
+
+    for (int i = 0; i < key; i++)
+    {
+        if (key > 26)
+        {
+            key = key % 26;
+        }
+    }
+
+    string plaintext = get_string("plaintext: ");
+    int caracteres = strlen(plaintext);
+    int cripto = 0;
+
+    for (int i = 0; i < caracteres; i++)
+    {
+        if (isalpha(plaintext[i]))
+        {
+            if (isupper(plaintext[i]))
+            {
+                cripto = plaintext[i] - 'A';
+                cripto = cripto + key;
+                cripto = cripto + 'A';
+
+                if(cripto > 90)
+                {
+                    cripto = cripto - 25;
+                }
+                printf("%c", cripto);
+            }
+
+            else if (islower(plaintext[i]))
+            {
+                cripto = plaintext[i] - 'a';
+                cripto = cripto + key;
+                cripto = cripto + 'a';
+
+                if(cripto > 122)
+                {
+                    cripto = cripto - 25;
+                }
+
+                printf("%c", cripto);
+            }
+        }
+
+        else
+        {
+            cripto = plaintext[i];
+            printf("%c", cripto);
+        }
+    }
+
+    return 0;
+}
